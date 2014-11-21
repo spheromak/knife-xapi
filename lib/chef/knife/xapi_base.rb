46ca73428c096eec6369b3516248786c789c91c4
Lennart Brinkmann <lebrinkma@gmail.com>
2014-11-21 18:20:53 +0100

    xapi_base.rb: fix print_vdi_info()
    remove usage of deprecated and now removed #bytes method
    of the Integer class.

--- lib/chef/knife/xapi_base.rb
+++ lib/chef/knife/xapi_base.rb
@@ -462,8 +462,8 @@ class Chef::Knife
       color_kv "  UUID: ",  record['uuid'], [:magenta, :cyan]
       color_kv "  Description: ", record['name_description'], [:magenta, :cyan]
       color_kv "  Type: ", record['type'], [:magenta, :cyan]
-      color_kv "  Size (gb): ", record['virtual_size'].to_i.bytes.to_gb.to_s, [:magenta, :cyan]
-      color_kv "  Utilized (gb): ", record['physical_utilisation'].to_i.bytes.to_gb.to_s, [:magenta, :cyan]
+      color_kv "  Size (gb): ", (record['virtual_size'].to_i / (1024 ** 3)).to_s, [:magenta, :cyan]
+      color_kv "  Utilized (gb): ", (record['physical_utilisation'].to_i / (1024 ** 3)).to_s, [:magenta, :cyan]
       record["VBDs"].each do |vbd|
         vm = xapi.VBD.get_VM(vbd)
         color_kv "    VM name: ",  xapi.VM.get_name_label(vm)
